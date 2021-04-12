2d6dd43730084977bd7be34cdeae3262f03cfd7a
Bui Huy <huy.bui91221@gmail.com>
2021-04-12 19:55:04 +0700

    Added physics and controls to new kart; improved steer physics

--- Assets/Karting/Scripts/KartSystems/ArcadeKart.cs
+++ Assets/Karting/Scripts/KartSystems/ArcadeKart.cs
@@ -556,7 +556,7 @@ namespace KartGame.KartSystems
                 }
 
                 // rotate our velocity based on current steer value
-                Rigidbody.velocity = Quaternion.AngleAxis(turningPower * Mathf.Sign(localVel.z) * velocitySteering * m_CurrentGrip * Time.fixedDeltaTime, transform.up) * Rigidbody.velocity;
+                //Rigidbody.velocity = Quaternion.AngleAxis(turningPower * Mathf.Sign(localVel.z) * velocitySteering * m_CurrentGrip * Time.fixedDeltaTime, transform.up) * Rigidbody.velocity;
             }
             else
             {
